72471420abf363b7c49af49781f3732fdf6416ae
Rohan Singh <65129632+rohansingh2023@users.noreply.github.com>
2023-04-22 01:03:17 +0530

    backend frontend connected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,12 @@
-import './App.css'
-import InputText from './components/InputText'
-import OutputText from './components/OutputText'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
-    <div><Navbar />
-
-      <div className="flex flex-col text-center transition-all">
-        <InputText />
-        <button className=' bg-cyan-300 p-5 w-1/4 m-auto rounded-md duration-300 hover:bg-cyan-400 hover:shadow-md hover:scale-105'>Get Summary</button>
-        <OutputText />
-      </div>
-
+    <div>
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/InputText.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InputText = () => {
-  return (
-    <textarea className='m-10 p-10 h-96 bg-slate-200 rounded-md shadow-lg outline-none' type='text'/>
-  )
-}
-
-export default InputText
