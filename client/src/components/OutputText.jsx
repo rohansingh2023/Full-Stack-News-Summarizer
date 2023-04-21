28bd6fed87ef2a2357d3fdba86019263bed5d99d
TejasPatne <tejaspatne18@gmail.com>
2023-04-21 16:11:51 +0530

    initial commit

---
+++ client/src/components/OutputText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OutputText = () => {
+  return (
+    <div className='m-10 p-10 max-h-fit bg-slate-200 rounded-md shadow-lg'>
+        <p></p>  
+        {/* add summary text to above p tag */}
+    </div>
+  )
+}
+
+export default OutputText
